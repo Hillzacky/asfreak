243e92a4f93e74e11ba3dd199390fed17346e040
hillzacky <hilmansyah@xhr.my.id>
2024-09-07 23:21:53 +0700

    .

---
+++ README.md
@@ -0,0 +1,37 @@
+
+# Asfreak
+`Open source autoscript for phreaker`
+
+## First Setup
+*Tested on Ubuntu 20.04*
+```bash
+bash -c "$(wget -qO- https://raw.githubusercontent.com/hillzacky/asfreak/main/setup.sh)"
+```
+
+## Required Ports
+|  SERVICE  |  PORT  |
+|---------- |--------|
+| Vmess WS CDN TLS | 443 |
+| Vmess WS CDN | 80 |
+| Vmess gRPC  | 443 |
+| Vless WS CDN TLS  | 443 |
+| Vless WS CDN  | 80 |
+| Vless XTLS Vision  | 443 |
+| Vless gRPC  | 443 |
+| Trojan WS CDN TLS  | 443 |
+| Trojan WS CDN | 80 |
+| Trojan TCP TLS| 443 |
+| Trojan gRPC  | 443 |
+| Shadowsocks WS CDN TLS | 443 |
+| Shadowsocks WS CDN | 80 |
+| Shadowsocks gRPC  | 443 |
+| Shadowsocks 2022 WS CDN TLS | 443 |
+| Shadowsocks 2022 WS CDN | 80 |
+| Shadowsocks 2022 gRPC  | 443 |
+| Nginx| 443 (Reverse Proxy) |
+| Auto Delete Expired User| ✅ |
+| DNS Setting | ✅ |
+
+
+[<img src="https://img.buymeacoffee.com/button-api/?text=Buy me a coffee&emoji=&slug=hillzacky&button_colour=BD5FFF&font_colour=ffffff&font_family=Cookie&outline_colour=000000&coffee_colour=FFDD00" />](https://www.buymeacoffee.com/hillzacky)
+
---
+++ menu/massxray.sh
@@ -0,0 +1,36 @@
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+clear
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e "             ${WB}----- [ Mass Xray  Menu ] -----${NC}            "
+echo -e "${BB}————————————————————————————————————————————————————————${NC}"
+echo -e ""
+echo -e " ${MB}[1]${NC} ${YB}Create Account Mass Xray${NC} "
+echo -e " ${MB}[2]${NC} ${YB}Trial Account Mass Xray${NC} "
+echo -e " ${MB}[3]${NC} ${YB}Extend Account Mass Xray${NC} "
+echo -e " ${MB}[4]${NC} ${YB}Delete Account Mass Xray${NC} "
+echo -e " ${MB}[5]${NC} ${YB}Check User Login${NC} "
+echo -e ""
+echo -e " ${MB}[0]${NC} ${YB}Kembali ke Menu${NC}"
+echo -e ""
+echo -e "${BB}———————————————————————————————————————————————————————${NC}"
+echo -e ""
+read -p " Select menu :  "  opt
+echo -e ""
+case $opt in
+1) clear ; add-xray ; exit ;;
+2) clear ; trialxray ; exit ;;
+3) clear ; extend-xray ; exit ;;
+4) clear ; del-xray ; exit ;;
+5) clear ; cek-xray ; exit ;;
+0) clear ; menu ; exit ;;
+x) exit ;;
+*) echo -e "${YB}Wrong input${NC}" ; sleep 1 ; allxray ;;
+esac
